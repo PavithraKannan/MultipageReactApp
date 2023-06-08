c8000d90278778869453f22215fc7bf7d35f9881
PavithraKannan <74839955+PavithraKannan@users.noreply.github.com>
2023-06-08 21:59:42 +0300

    Add files via upload
    Uploaded MultiPageReact app

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function App() {
+  const [quote, setQuote] = useState('');
+  const handleRefreshClick = () => {
+    window.location.reload();
+  };
+
+  useEffect(() => {
+    const fetchQuote = async () => {
+      try {
+        const response = await axios.get('https://api.breakingbadquotes.xyz/v1/quotes/5');
+        setQuote(response.data[0].quote);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchQuote();
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Breaking Bad Quotes</h1>
+      <p>{quote}</p>
+      <button onClick={ handleRefreshClick }>Next</button>
+    </div>
+  );
+}
+export default App;
